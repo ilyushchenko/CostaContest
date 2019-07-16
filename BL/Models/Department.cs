21bf12cf8cb0058f086a153e611edfa4312c4df6
Pavel Ilyushchenko <pavel.ks1996@gmail.com>
2019-07-16 12:04:03 +0300

    Added models

---
+++ BL/Models/Department.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace BL.Models
+{
+    public class Department
+    {
+        public Guid Id { get; set; }
+
+        public Department ParentDepartmen {get;set;}
+
+        public Guid ParentDepartmentId { get; set; }
+
+        public string Code { get; set; }
+
+        public string Name { get; set; }
+    }
+}
---
+++ BL/Models/Employee.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace BL.Models
+{
+    class Employee
+    {
+        public Guid Id { get; set; }
+
+        public Guid DepartmentId { get; set; }
+        public Department Department { get; set; }
+        public string SurName { get; set; }
+        public string FirstName { get; set; }
+        public string Patronymic { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string DocSeries { get; set; }
+        public string DocNumber { get; set; }
+        public string Position { get; set; }
+
+        public int Age
+        {
+            get
+            {
+                var nowDate = DateTime.Today;
+                var age = nowDate.Year - DateOfBirth.Year;
+                if (DateOfBirth > nowDate.AddYears(-age)) age--;
+                return age;
+            }
+        }
+
+
+    }
+}
